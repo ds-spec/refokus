4a707c0d6960ffd2950520d7740286e704ae4eeb
Deepak <ds262969@gmail.com>
2025-02-02 18:53:24 +0530

    Navbar and Work Screen completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/NAvbar";
+import Work from "./components/Work";
 
 const App = () => {
   return (
-    <div className="w-full h-screen bg-zinc-900 text-white">
+    <div className='w-full font-["satoshi"] h-screen bg-zinc-900 text-white'>
       <Navbar />
+      <Work />
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,35 @@
 import React from "react";
+import Button from "./Button";
 
 const Navbar = () => {
   return (
-    <div className="max-w-screen-xl mx-auto py-6 bg-zinc-800 flex items-center">
-      <img
-        src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg"
-        alt=""
-      />
-      <div className="links flex gap-14 ml-20">
-        {["Home", "Work", "Culture"].map((elem) => (
-          <a href="#" className="text-sm">
-            {elem}
-          </a>
-        ))}
+    <div className="max-w-screen-xl mx-auto py-6 bg-zinc-900 flex items-center justify-between border-b-[1.8px] border-zinc-700">
+      <div className="left flex items-center">
+        <img
+          src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg"
+          alt=""
+        />
+        <div className="links flex gap-14 ml-20">
+          {["Home", "Work", "Culture", "", "News"].map((elem, index) => (
+            <a href="#" className="text-sm flex items-center gap-1.5">
+              {elem?.length === 0 ? (
+                <span className="w-[0.5px] h-8 bg-zinc-500"></span>
+              ) : (
+                <a href="#" className="text-sm flex items-center gap-2">
+                  {index === 1 && (
+                    <span
+                      style={{ boxShadow: "rgb(0, 255, 25) 0px 0px 0.55em" }}
+                      className="inline-block w-1.5 h-1.5 bg-green-500 rounded-full"
+                    ></span>
+                  )}
+                  {elem}
+                </a>
+              )}
+            </a>
+          ))}
+        </div>
       </div>
+      <Button />
     </div>
   );
 };
